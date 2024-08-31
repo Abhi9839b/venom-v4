87137f8a5af98f8de3e91fc64136418552ec604b
Abhi9839b <abhishekmaurya894825@gmail.com>
2024-08-31 20:11:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7291307729:AAGNycdqhsR4ix1bab7ApYp0Wxvw5v8nROo")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@A90abhi")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1383069784")
 
 
 
